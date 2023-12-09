f3a490c2712f4f35e99e591ab24813355d1165b0
Ratul Islam <iratul010@gmail.com>
2023-12-09 23:42:20 +0600

    route-setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
+import GlobalStyles from "../src/styles/GlobalStyles";
 function App() {
   return (
-    <div>
-      travel world
-    </div>
-  )
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="home" />} />
+          <Route path="home" element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
